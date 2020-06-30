9fd41de0a64cb3c35613481174aecf85377df160
Elton Zheng <elton.zheng.sh@gmail.com>
2020-06-30 22:45:36 +0800

    增加html-webpack-plugin插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development', // development 或 production
 
     entry: __dirname + "/app/main.js", //入口文件，来作为构建其内部依赖图的开始。进入入口起点后，webpack 会找出有哪些模块和库是入口起点（直接和间接）依赖的。
     output: {
-        path: __dirname + "/public",
+        path: __dirname + "/dist",
         filename: "bundle.js"
     },
     // “__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
 
     devtool: 'eval-source-map',
     devServer: {
-        contentBase: './public',
+        contentBase: './dist',
         historyApiFallback: true, //不跳转
         inline: true //实时刷新
     },
@@ -39,5 +42,15 @@ module.exports = {
                   loader: 'postcss-loader' // 处理scss之类的文件
                 }]
         }]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: 'Copyright 2020 Elton Zheng'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template: __dirname + '/app/index.tmpl.html',
+            filename: __dirname + '/dist/index.html'
+        })
+    ]
 }
